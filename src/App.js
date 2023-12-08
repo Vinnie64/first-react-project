56e884b7fd0c7644c2549441002944684b5d6f84
Vinnie <47307150+Vinnie64@users.noreply.github.com>
2023-12-08 11:54:48 -0500

    'Website' will now update to say Hello, <textbox value>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   const myname = 'Vinnie'
+
+   const [nameValue,setNameValue] = useState('');
+   const inputChange = (event) => {
+    setNameValue(event.target.value);
+   };
+
+   return ( 
+    <div className="App"> 
+    <input type="text" palceholder="Type your name..." value={nameValue} onChange = {inputChange}></input>
+    
+    <h1>Hello, {nameValue}</h1>
+    
     </div>
-  );
+   );
 }
 
 export default App;
